987bb48c06e50918734e19b45f8157d9437ae2cc
patryk.gonet <patryk.gonet@NY-03>
2024-03-24 12:24:54 +0100

    Add project files.

---
+++ Pracownicy/Pracownik.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pracownicy
+{
+    public abstract class Pracownik
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public decimal salary { get; set; }
+
+        public abstract void showInformation();
+
+        public virtual decimal getAnnualSalary()
+        {
+            return salary * 12;
+        }
+    }
+
+    public class Manager : Pracownik
+    {
+        public decimal annualBonus { get; set; }
+
+        public override void showInformation()
+        {
+            Console.WriteLine($"Manager - Imię: {firstName}, Nazwisko: {lastName}, Wynagrodzenie miesięczne: {salary}zł, Roczny bonus: {annualBonus}zł, Wynagrodzenie roczne: {this.getAnnualSalary() + this.annualBonus}zł");
+            Console.WriteLine("\r\n");
+        }
+    }
+
+    public class PracownikBiurowy : Pracownik
+    {
+        public int amountOfWorkHours { get; set; }
+
+        public override void showInformation()
+        {
+            Console.WriteLine($"Pracownik biurowy - Imię: {firstName}, Nazwisko: {lastName}, Wynagrodzenie miesięczne: {salary}zł, Ilość godzin pracy: {amountOfWorkHours}, Wynagrodzenie roczne: {this.getAnnualSalary()}zł");
+            Console.WriteLine("\r\n");
+        }
+    }
+
+    public class PracownikZdalny : Pracownik
+    {
+        public int amountOfRemoteDays { get; set; }
+
+        public override void showInformation()
+        {
+            Console.WriteLine($"Pracownik zdalny - Imię: {firstName}, Nazwisko: {lastName}, Wynagrodzenie miesięczne: {salary}zł, Ilość dni zdalnych: {amountOfRemoteDays}, Wynagrodzenie roczne: {this.getAnnualSalary()}zł");
+            Console.WriteLine("\r\n");
+        }
+    }
+}
+
---
+++ Pracownicy/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Pracownicy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PracownikBiurowy pracownik1 = new PracownikBiurowy
+            { 
+                firstName = "Jan",
+                lastName = "Kowalski",
+                salary = 4265.15m,
+                amountOfWorkHours = 375
+            };
+
+            PracownikZdalny pracownik2 = new PracownikZdalny
+            {
+                firstName = "Wojciech",
+                lastName = "Nowak",
+                salary = 5250.65m,
+                amountOfRemoteDays = 170
+            };
+
+            Manager manager1 = new Manager
+            {
+                firstName = "Łukasz",
+                lastName = "Kowalski",
+                salary = 10650.13m,
+                annualBonus = 3000
+            };
+
+            pracownik1.showInformation();
+            pracownik2.showInformation();
+            manager1.showInformation();
+
+            Thread.Sleep(1000 * 10);
+        }
+    }
+}
